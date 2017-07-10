13484c60d1e216175bb84f336f2e0cf720072599
fanwenzh <317814529@qq.com>
2017-07-10 21:55:13 +0800

    0710

--- js-es5/7-chapter-1.js
+++ js-es5/7-chapter-1.js
@@ -71,5 +71,3 @@ xhr.addEventListener("readystatechange", function() {}) // DOM2
 
 // XML(Extensible Markup Language) 可扩展标记语言
 // 利用自己扩展的规则标记来存储相关的数据
-
-// JSONP(JSON with Padding)
--- js-es5/7-chapter-3/server.js
+++ js-es5/7-chapter-3/server.js
@@ -8,6 +8,31 @@ var server = http.createServer(function(req, res) {
         pathname = urlObj.pathname,
         query = urlObj.query;
 
+     //->如果客户端请求的是静态的资源文件(HTML/CSS/JS/图片/音视频...),我们把文件中的内容获取到,并且返回给客户端进行渲染
+    var reg = /\.(HTML|JS|CSS)/i;
+    if (reg.test(pathname)) {
+        //->获取请求文件的后缀名
+        var suffix = reg.exec(pathname)[1].toUpperCase();
+
+        //->根据后缀名获取MIME的类型
+        var suffixType = "text/plain";
+        switch (suffix) {
+            case "HTML":
+                suffixType = "text/html";
+                break;
+            case "JS":
+                suffixType = "text/javascript";
+                break;
+            case "CSS":
+                suffixType = "text/css";
+                break;
+        }
+
+        //->读取对应文件中的代码,并且返回给客户端
+        var conFile = fs.readFileSync("." + pathname, "utf8");
+        response.writeHead(200, {'content-type': suffixType + ";charset=utf-8;"});
+        response.end(conFile);
+    }
     // API 处理接口处理
     var con = null,
         result = null,
@@ -31,7 +56,18 @@ var server = http.createServer(function(req, res) {
         res.end(JSON.stringify(result));
         return;
     }
+    if (pathname === "/getAll") {
+        // 接收客户端传进来的函数名
+        var fnName = query["callback"]
+            // 返回数据及函数
+        var con = JSON.parse(fs.readFileSync("./custom.json", "utf-8"));
+        res.writeHead(200, { "content-type": "text/javascript; chartset=utf-8;" });
+        res.end(fnName + '(' + con + ')');
+    }
 });
+
+// JSONP的处理
+
 
 server.listen(81, function() {
     console.log("server is listening on 81 port!");
